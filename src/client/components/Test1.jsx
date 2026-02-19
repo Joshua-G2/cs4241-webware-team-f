a82907b358e8c8f54706374902260f5a965305be
Joshua G. <124004980+Joshua-G2@users.noreply.github.com>
2026-02-19 18:22:15 -0500

    add tailwind css

--- src/client/components/Test1.jsx
+++ src/client/components/Test1.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 // import "../   ... .css";
+import reactLogo from '/assets/react.svg'
+import viteLogo from '/assets/vite.svg'
 import { useNavigate } from 'react-router-dom'; //react router
 
 
 
 function Test1() {
-    const [item, setItem] = useState("Item");
+    const [count, setCount] = useState(0);
     const navigate = useNavigate()
 
     const handleClick = async (e) => {
@@ -18,6 +20,29 @@ function Test1() {
         <div>
             <p>This is page 1!</p>
             <button className="button" onClick={handleClick}>Swap</button>
+
+            <br/>
+
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/client/App.jsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
         </div>
     );
 }
